57e384f0ba7372089e46b8e6eed5f20a5ee3cf7b
KerelosNasser <keronaser2030@gmail.com>
2026-06-09 15:42:15 +0300

    fix(android): move dynamic namespace injection before evaluationDependsOn

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,16 +14,7 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-subprojects {
     afterEvaluate {
         try {
             val android = project.extensions.getByName("android")
@@ -35,4 +26,11 @@ subprojects {
             }
         } catch (e: Exception) {}
     }
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
